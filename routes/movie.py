f3785ce88914dbc9b716dcb244a54bfe178b481b
tungphan311 <thanhtunga1lqd@gmail.com>
2020-06-19 18:38:53 +0700

    update get movie api

--- routes/movie.py
+++ routes/movie.py
@@ -1,4 +1,4 @@
-from app.models import Movie, Rating, Review, User
+from app.models import Movie, Rating, Review, User, Favorite
 from app.response import create_response
 from app import db
 import datetime
@@ -10,6 +10,8 @@ def movie_get_by_id(id, user_id):
     ratings = Rating.query.filter_by(movie_id=movie.id).all()
     reviews = Review.query.filter_by(movie_id=movie.id).all()
     rated = Rating.query.filter_by(movie_id=movie.id, user_id=user_id).first()
+    favorite = Favorite.query.filter_by(
+        movie_id=movie.id, user_id=user_id).first()
 
     # calculate avarage rating of movie with request id
     total_rating = 0
@@ -20,11 +22,14 @@ def movie_get_by_id(id, user_id):
     # return score of user_id rate movie_id
     user_rate = 0 if rated is None else rated.rating
 
+    isFavorite = True if favorite is not None else False
+
     res = {
         "rating": avg_rating,
         "total_rating": len(ratings),
         "reviews": reviews,
-        "user_rate": user_rate
+        "user_rate": user_rate,
+        "is_favorite": isFavorite
     }
 
     mes = "Get movie's info with id = " + str(id) + " successfully."
