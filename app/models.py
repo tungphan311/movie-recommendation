0a22bb11ba081216a33cec6a3d293e9f7b1966b9
tungphan311 <thanhtunga1lqd@gmail.com>
2020-07-15 07:26:00 +0700

    remove env

--- app/models.py
+++ app/models.py
@@ -46,11 +46,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True)
     password_hash = db.Column(db.String(128))
-    ratings = db.relationship('Rating', backref='author', lazy='dynamic')
-    reviews = db.relationship('Review', backref='review', lazy='dynamic')
-    views = db.relationship('View', backref='view', lazy='dynamic')
-    favorites = db.relationship('Favorite', backref='author', lazy='dynamic')
-    recommends = db.relationship('Recommend', backref='target', lazy='dynamic')
+    # ratings = db.relationship('Rating', backref='author', lazy=True)
+    # reviews = db.relationship('Review', backref='review', lazy=True)
+    # views = db.relationship('View', backref='view', lazy=True)
+    # favorites = db.relationship('Favorite', backref='author', lazy=True)
+    # recommends = db.relationship('Recommend', backref='target', lazy=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -64,68 +64,163 @@ class User(db.Model):
 
 class Recommend(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=True)
+    movie_id = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
         return '<Recommend {}>'.format(self.movie_id)
-
 
 class Movie(SearchableMixin, db.Model):
     __tablename__ = 'movie'
     __searchable__ = ['title']
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
-    genres = db.Column(db.String(200))
+    original_title = db.Column(db.String(128))
     tmdb_id = db.Column(db.Integer)
     rating = db.Column(db.Float)
-    ratings = db.relationship('Rating', backref='movie', lazy='dynamic')
-    reviews = db.relationship('Review', backref='movie_review', lazy='dynamic')
-    views = db.relationship('View', backref='movie_view', lazy='dynamic')
-    favorites = db.relationship('Favorite', backref='movie', lazy='dynamic')
-    recommends = db.relationship('Recommend', backref='movie', lazy='dynamic')
+    vote_average = db.Column(db.Float)
+    vote_count = db.Column(db.Integer)
+    backdrop_path = db.Column(db.String(500))
+    poster_path = db.Column(db.String(500))
+    release_date = db.Column(db.String(500))
+    runtime = db.Column(db.Integer)
+    overview = db.Column(db.String)
+    certification = db.Column(db.String(120))
+    credit_id = db.Column(db.Integer)
+    # ratings = db.relationship('Rating', backref='movie', lazy=True)
+    # reviews = db.relationship('Review', backref='movie_review', lazy=True)
+    # views = db.relationship('View', backref='movie_view', lazy=True)
+    # favorites = db.relationship('Favorite', backref='movie', lazy=True)
+    # recommends = db.relationship('Recommend', backref='movie', lazy=True)
+    # genres = db.relationship('Genre', backref='movie', lazy=True)
+    # keywords = db.relationship('Keyword', backref='movie', lazy=True)
+    # credit = db.relationship('Credit', uselist=False, back_populates='movie')
+    # videos = db.relationship('Video', backref='movie', lazy=True)
 
     def __repr__(self):
         return '<Movie {}>'.format(self.ratings)
 
+class MovieGenres(db.Model):
+    __tablename__ = 'movie_genres'
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer)
+    genre_id = db.Column(db.Integer)
+    # movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    # genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'), nullable=False)
+
+class MovieKeywords(db.Model):
+    __tablename__ = 'movie_keywords'
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer)
+    keyword_id = db.Column(db.Integer)
+    # movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    # keyword_id = db.Column(db.Integer, db.ForeignKey(
+    #     'keyword.id'), nullable=False)
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Genre {}>'.format(self.name)
+
+class Keyword(db.Model):
+    __tablename__ = 'keyword'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Keyword {}>'.format(self.name)
+
+class Credit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class CreditCrews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    credit_id = db.Column(db.Integer)
+    crew_id = db.Column(db.Integer)
+
+class CreditCasts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    credit_id = db.Column(db.Integer)
+    cast_id = db.Column(db.Integer)
+    order = db.Column(db.Integer)
+
+class Crew(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    department = db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    # credit_id = db.Column(db.Integer, db.ForeignKey('credit.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Crew {}>'.format(self.name)
+
+class Cast(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character = db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    image = db.Column(db.String(120))
+    # credit_id = db.Column(db.Integer, db.ForeignKey(
+    #     'credit.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Cast {}>'.format(self.name)
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    movie_id = db.Column(db.Integer, nullable=False)
+    # movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Video {}>'.format(self.name)
 
 class Rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<Rating {}>'.format(self.rating)
-
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     headline = db.Column(db.String(128))
     body = db.Column(db.String(500))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<Review {}>'.format(self.body)
 
-
 class View(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
 
     def __repr__(self):
         return '<View {}>'.format(self.user_id)
 
-
 class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
 
     def __repr__(self):
         return '<Favorite {}>'.format(self.user_id)
--- config.py
+++ config.py
@@ -13,7 +13,13 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    API_KEY = os.environ.get('API_KEY')
+
     PAGE_SIZE = 20
 
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     CORS_HEADERS = 'Content-Type'
+
+    IMG_URL = "http://images.tmdb.org/t/p/original"
+    IMG_DEFAULT = "https://www.reelviews.net/resources/img/default_poster.jpg"
+    BACKDROP_DEFAULT = "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
