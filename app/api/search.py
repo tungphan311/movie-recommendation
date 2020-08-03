e2e37501cdf15c37bca41cb7b07e9afb3756263f
tungphan311 <thanhtunga1lqd@gmail.com>
2020-08-03 22:06:08 +0700

    title search

--- app/api/search.py
+++ app/api/search.py
@@ -1,13 +1,18 @@
 from app import app
 from flask import request
 from routes.search import search_movie
+from flask_jwt_extended import jwt_required
+from app.api.helpers import get_authorization
 
 @app.route("/api/movies")
+@jwt_required
 def search():
+    user_id, _ = get_authorization()
+
     data = request.args
     page = data.get("page", 1, type=int)
     key = data.get("query", "", type=str)
     searchType = data.get("type", "All", type=str)
     short = data.get("short", 1, type=int)
 
-    return search_movie(key, page, searchType, short)
+    return search_movie(key, page, searchType, short, user_id)
--- routes/search.py
+++ routes/search.py
@@ -3,19 +3,18 @@ from app.response import create_response
 from app.search import query_index
 from app import app, db
 from routes.recommendation import get_movie_by_id
+from routes.movie import get_movie
 from sqlalchemy import desc, case
 from unidecode import unidecode
 
 
-def search_movie(key, page, searchType, short):
-    page_size = app.config['PAGE_SIZE']
-
+def search_movie(key, page, searchType, short, user_id):
     newKey = unidecode(key)
 
     response = { 'query': key }
 
     if searchType == 'All' or searchType == 'Titles':
-        movies = query_movie(newKey, page, short)
+        movies = query_movie(newKey, page, short, user_id)
         response['title'] = movies
 
     if searchType == 'Celebs' or searchType == 'All':
@@ -25,22 +24,26 @@ def search_movie(key, page, searchType, short):
     return create_response(200, 'Success', data=response)
 
 
-def query_movie(key, page, short):
+def query_movie(key, page, short, user_id):
+    page_size = app.config['PAGE_SIZE']
+
     # create query
     search1 = "{}%".format(key)
     search2 = "%{}%".format(key)
 
-    result = []
 
     if short == 1:
+        result = []
         movies = Movie.query.filter(Movie.title.like(search1))\
                 .order_by(Movie.rating.desc())\
                 .limit(3)\
                 .all()
 
         for movie in movies:
-            res = get_movie(movie.id)
+            res = get_movie_short(movie.id)
             result.append(res)
+
+        return result
     else:
         # order result by priory: title starts with key -> title contains key
         query = Movie.query.\
@@ -52,10 +55,24 @@ def query_movie(key, page, short):
                     else_=3))\
             .paginate(page, page_size, error_out=False)
 
-        for q in query.items:
-            response.append(q.id)
+        total = Movie.query.\
+            filter(Movie.title.like(search1), Movie.title.like(search2))\
+            .order_by(
+                case(
+                    [(Movie.title.like(search1), 0),
+                        (Movie.title.like(search2), 1)],
+                    else_=3))\
+            .count()
 
-    return result
+        response = []
+        
+        for q in query.items:
+            res = get_movie(q.id, user_id)
+            response.append(res)
+
+        has_more = True if total > page_size else False
+
+        return { 'has_more': has_more, 'list': response }
 
 
 def query_cast(key, page, short):
@@ -74,7 +91,7 @@ def query_cast(key, page, short):
 
 
 
-def get_movie(id):
+def get_movie_short(id):
     movie = Movie.query.get(id)
     release_date = movie.release_date
     year = release_date[:4]
