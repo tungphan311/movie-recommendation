1ae0daeecd34b22f74dee0ff9a44237654808667
tungphan311 <thanhtunga1lqd@gmail.com>
2020-08-04 03:58:00 +0700

    get actor's movie

--- app/api/search.py
+++ app/api/search.py
@@ -3,6 +3,7 @@ from flask import request
 from routes.search import search_movie
 from flask_jwt_extended import jwt_required
 from app.api.helpers import get_authorization
+from routes.actor import actor_get_movies
 
 @app.route("/api/movies")
 @jwt_required
@@ -16,3 +17,14 @@ def search():
     short = data.get("short", 1, type=int)
 
     return search_movie(key, page, searchType, short, user_id)
+
+
+@app.route("/api/actor/<int:id>")
+@jwt_required
+def actor_id(id=0):
+    user_id, _ = get_authorization()
+
+    data = request.args
+    page = data.get("page", 1, type=int)
+
+    return actor_get_movies(id, page, user_id)
